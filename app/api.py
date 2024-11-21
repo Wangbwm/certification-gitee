bf24534ed2db69e207b8d5fc00ec390221e62cbb
Wangbw <1259963577@qq.com>
2024-11-21 14:31:39 +0800

    更新

--- app/api.py
+++ app/api.py
@@ -40,7 +40,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 24 * 60
 INIT_PASSWORD = "password"
 
 # 照片文件夹
-PHOTO_DIR = "static/photos"
+PHOTO_DIR = "app/static/photos"
 
 # OAuth2PasswordBearer会创建一个依赖项来验证令牌
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
